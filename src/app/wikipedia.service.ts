954a10bce8aeabc974faa2bc8d8a423e86e5065d
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-27 22:37:07 +1100

    172. Accessing Services

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WikipediaService {
+  constructor() {}
 
-  constructor() { }
+  search(term: string) {
+    return 'I am wikipedia search results';
+  }
 }
+
+// https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space
