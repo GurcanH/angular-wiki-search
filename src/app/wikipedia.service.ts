eb9101d33d6b851cf36720116439ac5420fabde0
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-27 23:33:32 +1100

    177. Making HTTP Requests

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WikipediaService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   search(term: string) {
-    return 'I am wikipedia search results';
+    return this.http.get(
+      'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space'
+    );
   }
 }
-
-// https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space
